1b51f72fe0c4e8b93ec9a861f80c4103261eaa96
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-10-24 14:46:53 +0200

    ADDED: creazione numeri random e pulsante per far iniziare il  gioco

--- js/script.js
+++ js/script.js
@@ -1,22 +1,42 @@
 
 
 const countDown = document.getElementById('countdown')
+const startButton = document.querySelector('.btn-success')
+const randomList = document.querySelector('ul.list-unstyled')
+const min = 1;
+const max = 100;
 
 
-
-
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is 
+  }
 
 startButton.addEventListener('click', () => {
     // Gestiamo i bottoni
     startButton.disabled = true;
-    stopButton.disabled = false;
+    for(i = 0;  i < 5; i++){
+        const randomNum = document.createElement('li')
+        randomNum.innerText = getRandomInt(min , max)
+        randomList.appendChild(randomNum)
+        
+    }
+    
   
     // Gestiamo il timer
-    count = 0;
+    count = 5;
     countDown.innerText = count;
   
     // incremento il tempo
-    timer = setInterval(() => {
-      countDown.innerText = ++count;
+    timer =setInterval(() => {
+      if (count === 0) {
+        document.querySelectorAll('ul.list-unstyled li').forEach(el => el.remove());
+        
+      } else {
+        
+        countDown.innerText = --count;
+      }
     }, 1000);
+    
   });
