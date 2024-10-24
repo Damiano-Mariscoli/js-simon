fd3a734733c31d47158daf0f307ebc035dca0300
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-10-24 16:44:29 +0200

    ADDED: logica di base

--- js/script.js
+++ js/script.js
@@ -2,19 +2,20 @@
 
 const countDown = document.getElementById('countdown')
 const startButton = document.querySelector('.btn-success')
+const confirmButton = document.querySelector('.btn-primary')
 const randomList = document.querySelector('ul.list-unstyled')
 const formNum = document.getElementById('answers-form')
 const instructions = document.getElementById('instructions')
+
 const min = 1;
 const max = 50;
-
 
 function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is 
   }
-
+let listAfter = document.querySelectorAll('ul.list-unstyled li')
 startButton.addEventListener('click', () => {
     // Gestiamo i bottoni
     startButton.classList.add('d-none')
@@ -24,7 +25,7 @@ startButton.addEventListener('click', () => {
         randomNum.innerText = getRandomInt(min , max)
         randomList.appendChild(randomNum)
         
-    }
+    }const listAfter = document.querySelectorAll('ul.list-unstyled li')
     
   
     // Gestiamo il timer
@@ -32,10 +33,40 @@ startButton.addEventListener('click', () => {
     countDown.innerText = count;
     // incremento il tempo
     timer =setInterval(() => {
+   
       if (count === 0) {
-        document.querySelectorAll('ul.list-unstyled li').forEach(i => i.remove());
+        console.log(listAfter)
+        listAfter.forEach(i => i.classList.add('d-none'));
         formNum.classList.remove('d-none')
-        instructions.innerText = "Scrivi i numeri che ricordi!";
+        instructions.innerText = "Scrivi i numeri che ricordi!"; 
+        clearInterval(timer);
+
+
+        confirmButton.addEventListener('click', () => {
+            let arrayNum = Array.from(listAfter).map(i => i.textContent)
+            console.log(arrayNum)
+
+            let arrayInput = Array.from(document.querySelectorAll('input')).map(i => i.value)
+            console.log(arrayInput)
+
+            let correctNum = 0;
+            arrayNum.forEach(num => {
+                if (arrayInput.includes(num)) {
+                    correctNum++;
+                }
+            });
+            console.log(`Hai azzeccato ${correctNum} numeri`);
+            if(arrayInput.length === arrayInput.length){
+                const result = `Hai azzeccato ${correctNum} numeri`;
+                instructions.innerText = result;
+                formNum.classList.add('d-none');
+                confirmButton.disabled = true;
+            }
+            
+            
+            
+                
+        })
       } else {
         
         countDown.innerText = --count;
@@ -43,3 +74,7 @@ startButton.addEventListener('click', () => {
     }, 1000);
     
   });
+
+  
+
+
